e8c36f72c27a59202d602cd72a9db9c984ac5b04
Odu Nkiruka <40614976+write2nk@users.noreply.github.com>
2018-10-12 07:37:16 -0700

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,54 +1,65 @@
-console.log('Service Worker: Registered');
+console.log("Service worker: registered");
+let name_cache = 'restaurant-v1';
 
-self.addEventListener('install', function(e){
-	e.waitUntil(
-		caches.open('v1').then(function(cache){
-			return cache.addAll(cacheFiles);
+self.addEventListener('Install', function (event) {
+	event.waitUntil(
+		caches.open(name_cache).then(function (cache) {
+			return cache.addAll([
+				'./',
+				'./index.html',
+				'./restaurant.html',
+				'./css/styles.css',
+				'./css/mobileCss.css',
+				'./css/laptop.css',
+				'./js/dbhelper.js',
+				'.data/restaurant.json',
+				'./js/main.js',
+				'./js/restaurant_info.js',
+				'/js/swRegister.js',
+				'./img/1.jpg',
+				'./img/2.jpg',
+				'./img/3.jpg',
+				'./img/4.jpg',
+				'./img/5.jpg',
+				'./img/6.jpg',
+				'./img/7.jpg',
+				'./img/8.jpg',
+				'./img/10.jpg'
+
+			], {
+				mode: 'no-cors'
+			});
 		})
-		);
+	);
 });
 
-const cacheFiles = [
-'/',
-'index.html',
-'/restaurant.html',
-'/css/styles.css',
-'/js/dbhelper.js',
-'/js/restaurant_info.js',
-'/data/restaurants.json',
-'/img/1.jpg',
-'/img/2.jpg',
-'/img/3.jpg',
-'/img/4.jpg',
-'/img/5.jpg',
-'/img/6.jpg',
-'/img/7.jpg',
-'/img/8.jpg',
-'img/9.jpg',
-'img/10.jpg'
-];
-
-self.addEventListener('fetch', function(e){
-	e.respondWith(
-		caches.match(e.request).then(function(response){
-			if(response){
-				console.log('Found', e.request, ' in cache');
-				return response;
-			}else{
-				console.log('Could not find ', e.request, ' in cache, FETCHING');
-				return fetch(e.request);
-				.then(function(response){
-					const clonedResponse = response.clone();
-					caches.open('v1').then(function(cache){
-						cache.put(e.request, clonedresponse);
+self.addEventListener('activate', function (event) {
+	event.waitUntil(
+		caches.keys()
+			.then(function (cacheNames) {
+				return Promise.all(
+					cacheNames.filter(function (cacheName) {
+						return cacheName.startsWith('restaurant-') &&
+							cacheName != name_cache;
+					}).map(function (cacheName) {
+						return caches.delete(cacheName);
 					})
-					return response;
-				})
-				.catch(function(err){
-					console.error(err);
-				});
+				);
+			})
+	);
+});
+
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		caches.match(event.request)
+		.then(function (response) {
+			if (response) {
+				return response;
 			}
+			return fetch(event.request);
+
+
 		})
-		);
+	);
 
 });

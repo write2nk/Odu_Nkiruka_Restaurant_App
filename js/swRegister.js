de9f17fed856d5c1340901ef90a120ed40225e87
Odu Nkiruka <40614976+write2nk@users.noreply.github.com>
2018-10-12 07:26:55 -0700

    Update swRegister.js

--- js/swRegister.js
+++ js/swRegister.js
@@ -1,9 +1,9 @@
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('./sw.js')
 	.then(function() {
-		console.log('Registration worked!');
+		console.log('Registration worked');
 	})
 	.catch(function() {
-		console.log('Registration failed!');
+		console.log('Registration failed');
 	});
 }

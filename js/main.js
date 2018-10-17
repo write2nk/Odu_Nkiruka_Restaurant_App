da61beff46155ec19ae6d5f8eebc094501fb0266
Odu Nkiruka <40614976+write2nk@users.noreply.github.com>
2018-10-17 15:52:34 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -160,11 +160,11 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.alt = restaurant.name;
+  image.alt = "Image of the restaurant " + restaurant.name;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
@@ -179,7 +179,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  //more.tabIndex = '3';
+  
   li.append(more)
 
   return li

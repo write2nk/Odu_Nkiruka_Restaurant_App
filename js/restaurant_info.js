a174dc54f5172148ca052f5e9efe7fbb0f8bc9ee
Odu Nkiruka <40614976+write2nk@users.noreply.github.com>
2018-10-17 15:46:33 -0700

    Update restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -88,7 +88,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img';
-  image.alt = image.className;
+  image.alt = "Image of the restaurant" + restaurant.name; //image.className;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
@@ -127,7 +127,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
-  const title = document.createElement('h2');
+  const title = document.createElement('h3');
   title.innerHTML = 'Reviews';
   container.appendChild(title);
 
